87699b71d99778d396729a370d1a39093673b7d0
Neville <nevil_dabre@yahoo.com>
2017-08-05 00:52:33 -0400

    active recipe

--- routes/getData.js
+++ routes/getData.js
@@ -1,10 +1,10 @@
 var recipe1 = require('../models/recipe1');
 
 exports.list = function (req, res) {
-    
+
     Promise.resolve()
         .then(function () {
-                return recipe1.find();
+            return recipe1.find();
         })
         .then(function (data) {
             if (data) {
@@ -18,13 +18,25 @@ exports.list = function (req, res) {
 
 exports.add = function (req, res) {
     var postData = Object.keys(req.query).length !== 0 ? req.query : Object.keys(req.body).length !== 0 ? req.body : null;
+
+    var entry = {
+        name: postData.name,
+        led1: postData.led1,
+        led2: postData.led2,
+        led3: postData.led3,
+        fadeUpTime: postData.fadeUpTime,
+        fadeUpDuration: postData.fadeUpDuration,
+        fadeDownTime: postData.fadeDownTime,
+        fadeDownDuration: postData.fadeDownDuration
+    };
+
     Promise.resolve()
         .then(function () {
             return recipe1.findOne();
         })
         .then(function (recipeData) {
             if (recipeData) {
-                return recipe1.findByIdAndUpdate(recipeData.id, postData);
+                return recipe1.findByIdAndUpdate(recipeData.id, entry);
             }
         })
         .then(function (recipeDataUpdate) {
